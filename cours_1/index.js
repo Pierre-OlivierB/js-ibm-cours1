b02883abe29156bed1087a8abb87e592deaf146d
unknown <baudinopierreolivier@gmail.com>
2023-01-12 11:11:47 +0100

    see some type of js

---
+++ cours_1/index.js
@@ -0,0 +1,37 @@
+let dudule='test';
+// console.log(dudule);
+// document.write(dudule);
+
+//----------------------------------
+//integer
+let day=3;
+let work= 20;
+let dayWork = day * work;
+console.log(typeof(dayWork));
+// typeof(dayWork);
+
+//string
+let day2="3 need by part ";
+let work2= "20 files";
+let dayWork2 = day2 +" for "+ work2;
+console.log(dayWork2);
+//---------------------------------------
+//Array
+let myBox  = new Array();
+myBox[0]=1;
+myBox[1]=0;
+myBox[2]=12;
+myBox[3]=8;
+console.log(myBox);
+//---------------------------------
+//Alert
+//alert(myBox[0])
+//---------------------------------
+//boolean
+let think = true;
+console.log(think);
+//-----------------------------------
+//Sort
+let myNewBox  = new Array();
+myNewBox = myBox.sort();
+console.log(myNewBox);
